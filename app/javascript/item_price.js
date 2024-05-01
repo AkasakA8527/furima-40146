162143f6ab4a5d29d2ae6582ed2baf1b1702b02b
AkasakA8527 <nakagawa.osamu@att.net>
2024-05-01 19:07:18 +0900

    JSが発火しない問題解決

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,16 +1,18 @@
-window.addEventListener('turbo:load', () => {
-  console.log("OK");
+document.addEventListener('DOMContentLoaded', function() {
+
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+
+    const inputValue = priceInput.value;
+
+    let addTaxValue = Math.floor(inputValue * 0.1);
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = addTaxValue;
+
+    let profitValue = inputValue - addTaxValue;
+    const addProfitDom = document.getElementById("profit");
+    addProfitDom.innerHTML = profitValue;
+
+  });
+
 });
-
-const priceInput = document.getElementById("item-price");
-priceInput.addEventListener("input", () => {
-  const inputValue = priceInput.value;
-
-  let addTaxValue = Math.floor(inputValue * 0.1);
-  const addTaxDom = document.getElementById("add-tax-price");
-  addTaxDom.innerHTML = addTaxValue;
-
-  let profitValue = inputValue - addTaxValue;
-  const addProfitDom = document.getElementById("profit");
-  addProfitDom.innerHTML = profitValue;
-})
