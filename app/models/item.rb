c88f2f6ccb01ad06a7f67cb3438b44fd6cfe7fa1
AkasakA8527 <nakagawa.osamu@att.net>
2024-05-01 16:23:33 +0900

    画像投稿まで成功版

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,6 +10,7 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipday
 
+  validates :image         , presence: true
   validates :item_name     , presence: true
   validates :description   , presence: true
   validates :category_id   , presence: true
@@ -18,6 +19,7 @@ class Item < ApplicationRecord
   validates :prefecture_id , presence: true
   validates :shipday_id    , presence: true
   validates :price         , presence: true
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
   validates :category_id, :condition_id, :whopay_id, :prefecture_id, :shipday_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
