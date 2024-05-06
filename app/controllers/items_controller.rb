d779b24a40e2a885e50eb999306a19e65f0f06bf
AkasakA8527 <nakagawa.osamu@att.net>
2024-05-06 13:37:51 +0900

    商品情報編集機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,10 +24,16 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless current_user == @item.user
+      redirect_to root_path
+    end
   end
 
   def update
     @item = Item.find(params[:id])
+    unless current_user == @item.user
+      redirect_to root_path and return
+    end
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
